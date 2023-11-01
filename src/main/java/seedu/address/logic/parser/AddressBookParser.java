d68f4a222589178d360db4263d458ccd466168a5
kayabuttertoastt <e0970063@u.nus.edu>
2023-11-01 11:20:25 +0800

    Implement Favlist feature
    Add:
    * Favlist command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,20 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FavouriteCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.FindCourseCommand;
-import seedu.address.logic.commands.FindRoleCommand;
-import seedu.address.logic.commands.FindTutorialCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.UnfavouriteCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -89,6 +76,9 @@ public class AddressBookParser {
 
         case FindTutorialCommand.COMMAND_WORD:
             return new FindTutorialCommandParser().parse(arguments);
+
+        case FavListCommand.COMMAND_WORD:
+            return new FavListCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
